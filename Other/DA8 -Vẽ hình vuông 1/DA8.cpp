a34cf6738d7319dc550e2d1fc4ad20372d8b05cb
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-24 00:13:31 +0700

    C++

---
+++ Other/DA8 -Vẽ hình vuông 1/DA8.cpp
@@ -0,0 +1,12 @@
+﻿#include <bits\stdc++.h>
+using namespace std;
+
+int main () {
+	int x; cin >> x;
+	for ( int i = 0; i < x; i++ ) {
+		int j = x;
+		while ( a-- ) {
+			cout << i << " ";
+		} cout << endl;
+	} system ( "pause" );
+}
