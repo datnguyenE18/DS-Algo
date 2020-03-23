039ef39ee598f1ace19abd94456753aa8a90382d
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-23 23:05:01 +0700

    C++

---
+++ Other/DA7 - Chuyển giây sang gio phut giay/C++.cpp
@@ -0,0 +1,8 @@
+﻿#include <bits\stdc++.h>
+using namespace std;
+
+int main () {
+	int sec, h, p; cin >> sec;
+	cout << sec / 120 << ":" << sec / 60 << ":" << sec - ( sec / 60 ) * 60 << endl;
+	system ( "pause" );
+}
