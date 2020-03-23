c6ef4b355ed5bc2eb209a978c9f19f29beccf284
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-24 00:24:51 +0700

    C++

---
+++ Other/DA9 - In bảng cửu chương/DA9.cpp
@@ -0,0 +1,11 @@
+﻿#include <bits\stdc++.h>
+using namespace std;
+
+int main () {
+	int i, j;
+	for ( i = 1; i <= 10; i++ ) {
+		for ( j = 1; j <= 10; j++ ) {
+			cout << i*j << "	";
+		} cout << "\n\n\n";
+	} system ( "pause" );
+}
