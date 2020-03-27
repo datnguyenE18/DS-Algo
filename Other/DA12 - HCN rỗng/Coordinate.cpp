0ae85ab54f1d98473fd96c8a7102b1f1aa2294af
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-27 19:54:56 +0700

    C++

---
+++ Other/DA12 - HCN rỗng/Coordinate.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main () {
+	int length, width;
+	cout << "Length: "; cin >> length;
+	cout << "Width: "; cin >> width;
+
+	for ( int i = 1; i <= length; i++ ) {
+		if ( i == 1 || i == length )
+			for ( int j = 1; j <= width; j++ ) cout << "*";
+
+		else {
+			for ( int j = 1; j <= width; j++ ) {
+				if ( j == 1 || j == width ) cout << "*";
+				else cout << " ";
+			}
+		} cout << endl;
+	} system ( "pause" );
+}
+
+
---
+++ Other/DA12 - HCN rỗng/Top - Mid - Top.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+//2 sides:
+void side ( int width ) {
+	for ( int i = 1; i <= width; i++ ) cout << "*";
+	cout << "\n";
+}
+
+//Mid:
+void mid ( int width ) {
+	for ( int i = 1; i <= width; i++ ) {
+		if ( i == 1 || i == width ) cout << "*";
+		else cout << " ";
+	} cout << "\n";
+}
+
+int main () {
+	int length, width;
+	cout << "Length: "; cin >> length;
+	cout << "Width: "; cin >> width;
+
+	side ( width );
+	for ( int i = 1; i <= length - 2; i++ ) mid ( width );
+	side ( width );
+	cout << endl;
+
+	system ( "pause" );
+}
+
+
