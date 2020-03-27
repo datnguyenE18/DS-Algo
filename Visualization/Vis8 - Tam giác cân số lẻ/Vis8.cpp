59ed2592eb200a00d5ebfef7973ad2465637af6b
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-27 22:52:01 +0700

    C++

---
+++ Visualization/Vis8 - Tam giác cân số lẻ/Vis8.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void doPrint ( char c, int num ) {
+	for ( int j = 1; j <= num; j++ ) cout << c;
+}
+
+void doPrintNum ( int c, int num ) {
+	for ( int j = 1; j <= num; j++ ) cout << c;
+}
+
+int main () {
+	int high; cin >> high;
+	int space = 0,
+		ast = high * 2 - 1,
+		num = 1;
+
+	for ( int i = 1; i <= high; i++ ) {
+		doPrint ( ' ', space );
+		doPrintNum ( num, ast );
+		cout << "\n";
+		space++;
+		ast -= 2;
+		num += 2;
+	} system ( "pause" );
+}
+
+
