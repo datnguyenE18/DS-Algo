f0efbb32a977063ed32ee5e2a5f8ece7db2e5ff6
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-27 22:17:13 +0700

    C++

---
+++ Other/DA13 - Hình chữ T/DA13.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+void doPrint ( char c, int num ) {
+	for ( int j = 1; j <= num; j++ ) cout << c;
+}
+
+int main () {
+	int high, large;
+	cin >> high >> large;
+
+	if ( large % 2 == 0 ) {
+		for ( int i = 1; i <= high; i++ ) {
+			if ( i == 1 || i == 2 ) {
+				doPrint ( '*', large );
+				cout << "\n";
+			} else {
+				doPrint ( ' ', large / 2 - 1 );
+				doPrint ( '*', 2 );
+				cout << "\n";
+			}
+		}
+	} else {
+		for ( int i = 1; i <= high; i++ ) {
+			if ( i == 1 ) {
+				doPrint ( '*', large );
+				cout << "\n";
+			} else {
+				doPrint ( ' ', large / 2 );
+				doPrint ( '*', 1 );
+				cout << "\n";
+			}
+		}
+	} system ( "pause" );
+}
+
+
