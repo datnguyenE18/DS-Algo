e61ffdea99f71dab4a894626dc9878484d633518
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-27 22:39:19 +0700

    C++

---
+++ Visualization/Vis7 - Tam giác cân ngược/DA.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void doPrint ( char c, int num ) {
+	for ( int j = 1; j <= num; j++ ) cout << c;
+}
+
+int main () {
+	int high; cin >> high;
+	int space = 0,
+		ast = high * 2 - 1;
+	for ( int i = high; i >= 1; i-- ) {
+		doPrint ( ' ', space );
+		doPrint ( '*', ast );
+		cout << "\n";
+		space++;
+		ast -= 2;
+	}
+	system ( "pause" );
+}
+
+
