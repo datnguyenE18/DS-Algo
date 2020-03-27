f7c0b0343b218a8b9ba5ef6915786b6e42711746
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-27 16:41:30 +0700

    C++

---
+++ Other/DA11 - Tam giác cân rỗng/Top - End.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main () {
+	int n;
+	cin >> n;
+
+	//Top
+	for ( int i = 1; i < n; i++ ) {
+		for ( int j = n - i; j > 0; j-- )
+			cout << " ";
+		for ( int k = 1; k <= 2 * i - 1; k++ ) {
+			if ( k == 2 * i - 1 || k == 1 )
+				cout << "*";
+			else cout << " ";
+		}
+		cout << endl;
+	}
+
+	//End:
+	for ( int i = 1; i <= 2 * n - 1; i++ ) cout << "*"; cout << "\n";
+	system ( "pause" );
+}
+
+
