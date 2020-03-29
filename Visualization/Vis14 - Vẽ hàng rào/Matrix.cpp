213e5751cbfabcbbb633741de278cf8ee1c5b087
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-29 11:38:42 +0700

    c++

---
+++ Visualization/Vis14 - Vẽ hàng rào/Matrix.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void doPrint ( char c, int num ) {
+	for ( int i = 1; i <= num; i++ ) cout << c;
+}
+
+int main () {
+	int hi, wi, spa;
+	cin >> hi >> wi >> spa;
+
+	//Head:
+	for ( int row = 1; row < hi; row++ ) {
+		for ( int colu = 1; colu <= wi; colu++ ) {
+			if ( colu == 1 || ( colu - 1 ) % spa == 0 ) cout << "*";
+			else cout << " ";
+		} cout << "\n";
+	}
+
+	//Tail:
+	doPrint ( '*', wi );
+	cout << "\n";
+	system ( "pause" );
+}
