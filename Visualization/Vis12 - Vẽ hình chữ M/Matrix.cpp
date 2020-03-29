02ebab3553bd3a1c6fc07300f8db14916c047b62
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-03-29 10:55:24 +0700

    C++

---
+++ Visualization/Vis12 - Vẽ hình chữ M/Matrix.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main () {
+	int high;
+	cin >> high;
+
+	for ( int c = 1; c <= high; c++ ) {
+		int r = 1;
+		if ( c <= high / 2 + 1 ) {
+			for ( ; r <= high; r++ ) {
+				if ( r == 1 || c == r || c + r == high + 1 || r == high ) cout << "*";
+				else cout << " ";
+			} cout << "\n";
+		} else {
+			for ( ; r <= high; r++ ) {
+				if ( r == 1 || r == high ) cout << "*";
+				else cout << " ";
+			} cout << "\n";
+		}
+	}system ( "pause" );
+}
---
+++ Visualization/Vis13 - Vẽ cái thang/C++.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+void doPrint ( char c, int num ) {
+	for ( int i = 1; i <= num; i++ ) cout << c;
+}
+
+void drawBlog ( int spa, int wi ) {
+	doPrint ( '*', wi );
+	cout << "\n";
+	for ( int i = 1; i <= spa - 1; i++ ) {
+		cout << "*";
+		doPrint ( ' ', wi - 2 );
+		cout << "*";
+		cout << "\n";
+	}
+}
+
+
+int main () {
+	int hi, wi, spa;
+	cin >> hi >> wi >> spa;
+
+	//top:
+	cout << "*";
+	doPrint ( ' ', wi - 2 );
+	cout << "*" << "\n";
+
+	//body
+	for ( int i = 1; i <= ( hi - 1 ) / spa; i++ ) drawBlog ( spa, wi );
+	if ( int rest = ( hi - 1 ) % spa ) drawBlog ( rest, wi );
+
+	system ( "pause" );
+}
