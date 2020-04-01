d4024b516c96f8b5ef70f8d7a9e65c519776d301
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-01 15:03:59 +0700

    C++

---
+++ Numerical/Nu11. Xâu nhị phân kế tiếp/Nu11.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits\stdc++.h>
+using namespace std;
+
+void solve ( string num ) {
+	int i = num.size () - 1;
+	for ( ; i >= 0; i-- ) {
+		if ( num[i] == '1' ) num[i] = '0';
+		else {
+			num[i] = '1';
+			cout << num << endl;
+			return;
+		}
+		if ( i == 0 ) cout << num << endl;
+	}
+}
+
+int main () {
+	int t; cin >> t;
+	string num;
+	while ( t-- ) {
+		cin >> num;
+		solve ( num );
+	} system ( "pause" );
+}
