7bcc976e76c800b171bf32d64f9b2b40524b714a
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-02 21:12:37 +0700

    C++

---
+++ Recurtion/Re2. Tìm chữ số lớn nhất của một số nguyên/Re2.cpp
@@ -0,0 +1,20 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int compare ( int a, int b ) {
+	if ( a > b ) return a;
+	return b;
+}
+
+int check ( int so ) {
+	if ( so == 0 ) return 0;
+	return compare ( so % 10, check ( so / 10 ) );
+}
+
+int main () {
+	int t, so; cin >> t;
+	while ( t-- ) {
+		cin >> so;
+		cout << check ( so ) << endl;
+	} system ( "pause" );
+}
