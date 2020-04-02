5848697af15bac13016241b80d7fff3f286c5098
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-02 20:45:04 +0700

    c++

---
+++ Recurtion/Re1. Tính số chữ số/Re1.cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int temp = 0;
+
+int count ( int so ) {
+	if ( so == 0 ) return temp;
+	temp++;
+	return count ( so / 10 );
+}
+
+int main () {
+	int t, so; cin >> t;
+	while ( t-- ) {
+		cin >> so;
+		cout << count ( so ) << endl;
+	} system ( "pause" );
+}
