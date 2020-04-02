39e8daddf34ef04f9b77d98ad8455711c6c4c61c
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-02 08:06:59 +0700

    C++

---
+++ Numerical/Nu15. [next_permutation] Hoán vị chữ số/Nu15.cpp
@@ -0,0 +1,12 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+	string begin, end;
+	cin >> begin;
+	end = begin;
+	next_permutation ( end.begin (), end.end () );
+	if ( begin < end ) cout << end << endl;
+	else cout << 0 << endl;
+	system ( "pause" );
+}
