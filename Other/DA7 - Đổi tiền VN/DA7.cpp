541787c5f80e67b49f216d86840728099203bae6
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-03 17:05:46 +0700

    C++

---
+++ Other/DA7 - Đổi tiền VN/DA7.cpp
@@ -0,0 +1,22 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int par[10] = { 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000 };
+int money, t;
+
+void exchag () {
+	for ( int i = 9; i >= 0; i-- ) {
+		if ( int count = money / par[i] ) {
+			cout << "(" << count << ")-" << par[i] << " ";
+			money = money % par[i];
+		}
+	} cout << "\n";
+}
+
+int main () {
+	cin >> t;
+	while ( t-- ) {
+		cin >> money;
+		exchag ();
+	} system ( "pause" );
+}
