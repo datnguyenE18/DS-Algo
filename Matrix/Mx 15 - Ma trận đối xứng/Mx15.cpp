04ebd01999b99b62c421e92ca3a1d8512d03e8ba
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-08 12:13:58 +0700

    C++

---
+++ Matrix/Mx 15 - Ma trận đối xứng/Mx15.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int t, n, x[111][111];
+
+void input () {
+	for ( int i = 0; i < n; i++ )
+	for ( int j = 0; j < n; j++ )
+		cin >> x[i][j];
+}
+
+int solve () {
+	for ( int i = 0; i < n; i++ ) {
+		for ( int j = 0; j < n; j++ ) {
+			if ( x[i][j] != x[j][i] ) return 0;
+		}
+	} return 1;
+}
+
+int main () {
+	cin >> t;
+	while ( t-- ) {
+		cin >> n;
+		input ();
+		if ( solve () ) cout << 1 << endl;
+		else cout << 0 << endl;
+	} system ( "pause" );
+}
