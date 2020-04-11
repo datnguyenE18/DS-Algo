3ee5ac48d5ba118da13df8b6f7f4eec63e73df6b
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-11 22:30:44 +0700

    C++

---
+++ Generation/Ge6. [prev_permutation] Liệt kê hoán vị ngược/Ge6.cpp
@@ -0,0 +1,34 @@
+﻿/*
+**generate_all_permutations_of_N_objects
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int t, n, temp;
+int x[1111111];
+
+void init () { //cấu hình đầu tiên
+	temp = n;
+	for ( int i = 0; i < n; i++ ) {
+		x[i] = temp;
+		temp--;
+	}
+}
+
+void display () { //In ra cấu hình hiện tại
+	for ( int i = 0; i < n; i++ ) cout << x[i];
+	cout << " ";
+}
+
+int main () {
+	cin >> t;
+	while ( t-- ) {
+		cin >> n;
+		init ();
+		do {
+			display ();
+		} while ( prev_permutation ( x, x + n ) );
+		cout << "\n";
+	} system ( "pause" );
+}
