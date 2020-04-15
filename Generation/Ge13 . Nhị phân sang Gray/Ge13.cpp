7119118ae23a9001ad7bc94f6f1f02ca4fb71e75
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-16 00:47:02 +0700

    C++

---
+++ Generation/Ge13 . Nhị phân sang Gray/Ge13.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+string binary;
+
+void print_gray () {
+	string shift = "0", gray = "";
+	shift += binary;
+	for ( int i = 0; i < binary.length (); i++ ) {
+		if ( binary[i] != shift[i] ) gray += "1";
+		else gray += "0";
+	}
+	cout << gray << endl;
+}
+
+int main () {
+	cin >> t;
+	while ( t-- ) {
+		cin >> binary;
+		print_gray ();
+	} system ( "pause" );
+}
