df21d0d4c7cc9f1bbada809b384ace99a6ee50f2
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-15 23:48:23 +0700

    C++

---
+++ Generation/Ge12. Mã Gray/Ge12. Đệ quy.cpp
@@ -0,0 +1,31 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+void print_gray ( string binary ) {
+	string shift = "0", gray = "";
+	shift += binary;
+	for ( int i = 0; i < binary.length (); i++ ) {
+		if ( binary[i] != shift[i] ) gray += "1";
+		else gray += "0";
+	} 
+	cout << gray << " ";
+}
+
+void bitString ( int x, string prefix ) {
+	if ( x == 0 ) print_gray ( prefix );
+
+	else {
+		bitString ( x - 1, ( prefix + "0" ) );
+		bitString ( x - 1, ( prefix + "1" ) );
+	}
+}
+
+int main () {
+	int n, t; cin >> t;
+
+	while ( t-- ) {
+		cin >> n;
+		bitString ( n, "" );
+		cout << "\n";
+	} system ( "pause" );
+}
---
+++ Generation/Ge12. Mã Gray/Ge12.cpp
@@ -0,0 +1,54 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+class Gray {
+	int m;
+	long n;
+	int A[65538];
+	public:
+	void nhap ();
+	void xuat ();
+
+};
+
+void Gray::xuat () {
+	int b[20];
+
+	for ( int i = 0; i<n; i++ ) {
+		int k = 0;
+		int u = A[i];
+		while ( u>0 ) {
+			b[k++] = u % 2;
+			u /= 2;
+		}
+		for ( int j = k + 1; j <= m; j++ ) cout << "0";
+		while ( k >= 1 ) cout << b[--k];
+		cout << " ";
+	}
+
+}
+
+void Gray::nhap () {
+	cin >> m;
+	A[0] = 0;
+	A[1] = 1;
+	int p = 1;
+	n = 2;
+	for ( int i = 2; i <= m; i++ ) {
+		p *= 2;
+		int u = 2 * n - 1;
+		for ( int j = n; j <= u; j++ ) A[j] = A[u - j] + p;
+		n *= 2;
+	}
+}
+
+int main () {
+	int t; cin >> t;
+	Gray G;
+	while ( t-- ) {
+		G.nhap ();
+		G.xuat ();
+		cout << "\n";
+	} system ( "pause" );
+
+}
