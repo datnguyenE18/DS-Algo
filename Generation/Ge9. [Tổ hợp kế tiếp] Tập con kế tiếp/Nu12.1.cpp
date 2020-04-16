71823600380617a88af277438f775bd18a66d8ea
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-16 19:56:00 +0700

    C++

--- Generation/Ge9. [Tổ hợp kế tiếp] Tập con kế tiếp/Nu12.1.cpp
+++ Generation/Ge9. [Tổ hợp kế tiếp] Tập con kế tiếp/Nu12.1.cpp
@@ -16,7 +16,7 @@ void next_Subset () {
 	int i = k; // duyệt từ cuối lên của tập con hiện tại
 	while ( i > 0 && x[i] == n - k + i ) i--; // tìm i sao cho x[i] khác n - k + i
 
-	if ( i > 0 ) { // nếu tìm i
+	if ( i > 0 ) { // nếu tìm thấy i
 		x[i] = x[i] + 1; // tăng x[i] lên một đơn vị
 		for ( int j = i + 1; j <= k; j++ )  // từ đó về sau mỗi phần tử tăng thêm (j - i) đơn vị
 			x[j] = x[i] + j - i;
