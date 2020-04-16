5004cc1c078793c762cc75ba59dc3dfec86cf04e
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-16 22:49:19 +0700

    C++

---
+++ Generation/Ge13. Binary to Gray/Ge13_ Cùng ý tưởng - khác cách trình bày.cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int t;
+string Binary;
+
+void Bin_to_Gray () {
+	cin >> Binary;
+	for ( int i = 0; i < Binary.length (); i++ ) {
+		if ( i == 0 ) cout << Binary[i];
+		else {
+			if ( Binary[i] != Binary[i - 1] ) cout << "1";
+			else cout << "0";
+		}
+	} cout << "\n";
+}
+
+int main () {
+	cin >> t;
+	while ( t-- ) {
+		Bin_to_Gray ();
+	} system ( "pause" );
+}
---
+++ Generation/Ge14. Gray to Binary/Ge14.cpp
@@ -0,0 +1,22 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int t;
+string gray;
+
+void Gray_to_Bin () {
+	string bin = "";
+	bin += gray[0];
+	for ( int i = 1; i < gray.length (); i++ ) {
+		if ( gray[i] != bin[i - 1] ) bin += "1";
+		else bin += "0";
+	} cout << bin << endl;
+}
+
+int main () {
+	cin >> t;
+	while ( t-- ) {
+		cin >> gray;
+		Gray_to_Bin ();
+	} system ( "pause" );
+}
