620f1a4e9cc294e9fc4dd23fd0d918443f321dc7
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-17 16:31:49 +0700

    C++

---
+++ Matrix/Mx16 - Nhập và in ma trận từ file/Mx16/Mx16.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100 //Kích thước tối đa của ma trận
+
+int n, // Kích thước ma trận 
+graph[MAX][MAX]; // Ma trận 2 chiều sẽ lưu ma trận đọc được
+
+void display ( ) {
+	ifstream FileIn ( "input.dat" ); //Khai báo biến, mở và đọc file 
+	FileIn >> n; // nhận kích thước ma trận
+
+	// Đọc ma trận
+	for ( int i = 0; i < n; i++ )
+	for ( int j = 0; j < n; j++ )
+		FileIn >> graph[i][j];
+
+	//In ma trận
+	for ( int i = 0; i < n; i++ ) {
+		for ( int j = 0; j < n; j++ )
+			cout << graph[i][j] << " ";
+		cout << "\n";
+	}
+	FileIn.close ( ); // đóng file
+}
+
+int main ( ) {
+	display ( );
+	system ( "pause" );
+}
---
+++ Matrix/Mx16 - Nhập và in ma trận từ file/Mx16_ cải tiến (Không cần biết kích thước cho trước)/Mx16_cải tiến.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define MAX 11111
+
+int graph[MAX][MAX], // Ma trận 2 chiều lưu ma trận đọc được
+size, //kích thước ma trận
+rows, cols; //hàng và cột
+string line;
+
+void reader ( string file ) {
+	ifstream FileIn ( file ); //Khai báo biến, mở và đọc file
+
+	for ( rows = 0; getline ( FileIn, line ); rows++ ) {
+		string element = "";
+		cols = 0; // quét từng cột một bắt đầu từ cột 0
+		for ( int t = 0; t < line.length (); t++ ) {
+			if ( line[t] != ' ' ) // Nếu ký tự hiện tại khác dấu cách => nối vào biến element
+				element += line[t];
+
+			if ( line[t] == ' ' || t == line.length () - 1 ) { // Nếu ký tự hiện tại là dấu cách, hoặc là ký tự cuối của hàng
+				graph[rows][cols] = atoi ( element.c_str () ); // Thực hiện chuyển string thành số int, hàm atoi chỉ nhận vào biến kiểu char*, nên cần dùng .c_str() để chuyển từ string về char*
+				cols++; // cột tiếp theo
+				element = ""; // Đặt lại giá trị element
+			}
+		}
+		size = cols; //Lấy luôn số phần tử của hàng đầu tiên làm kích thước ma trận để thực hiện hàm Display
+	}
+}
+
+
+void Display () {
+	for ( int i = 0; i < size; ++i ) {
+		for ( int j = 0; j < size; j++ )
+			cout << graph[i][j] << " ";
+		cout << "\n";
+	}
+}
+
+
+int main () {
+	reader ( "input.dat" );
+	Display ();
+	system ( "pause" );
+}
