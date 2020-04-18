e71652d4874578e2a622843b565a4eb0e13567a5
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-18 21:26:48 +0700

    C++

---
+++ 4. Generation/Ge3.2. Xâu AB đặc biệt/Ge3.2.cpp
@@ -0,0 +1,60 @@
+﻿#include<iostream>
+using namespace std;
+
+bool check = false;
+int n, k, count = 0; //count là biến đếm kết quả.
+char a[17];
+string s[1000000]; //chứa các dãy thỏa mãn
+
+//cấu hình ban đầu
+void init () {
+	for ( int i = 1; i <= n; i++ )
+		a[i] = 'A';
+}
+
+//Kiểm tra tính hợp lệ của chuỗi
+bool kt () {
+	int d = 0, t = 1;
+	for ( int i = 1; i < n; i++ ) {
+		if ( a[i] == 'A' && a[i + 1] == 'A' ) {
+			t++;
+			if ( t >= k ) d++;
+		} else t = 1;
+	}
+
+	if ( d == 1 ) return true;
+	else return false;
+}
+
+//Lưu chuỗi hợp lệ
+void dem () {
+	if ( kt () ) {
+		s[count] = "";
+		for ( int i = 1; i <= n; i++ )
+			s[count] += a[i];
+		count++;
+	}
+}
+
+void sinh () {
+	int i = n;
+	while ( a[i] == 'B' ) {
+		a[i] = 'A';
+		i--;
+	}
+	if ( i == 0 ) check = true;
+	else a[i] = 'B';
+}
+
+int main () {
+	cin >> n >> k;
+	init ();
+	while ( !check ) {
+		sinh ();
+		dem ();
+	}
+	cout << count << endl;
+	for ( int i = 0; i < count; i++ ) {
+		cout << s[i] << endl;
+	} system ( "pause" );
+}
---
+++ 4. Generation/Ge3.2. Xâu AB đặc biệt/Ge3.2_C2.cpp
@@ -0,0 +1,49 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define M 100
+
+int n, k, d = 0;
+vector<vector<int> > v;
+vector<int> x;
+
+void Update () {
+	v.push_back ( x );
+	d++;
+}
+
+void res () {
+	for ( int i = v.size () - 1; i >= 0; i-- ) {
+		for ( int j = 0; j < v.at ( i ).size (); j++ ) {
+			if ( v.at ( i ).at ( j ) == 1 ) cout << "A";
+			else cout << "B";
+		} cout << endl;
+	}
+}
+
+bool Check () {
+	int t = 0, dem = 0;
+	for ( int i = 0; i <= n - 1; i++ ) {
+		if ( x.at ( i ) == 1 ) t++;
+		else t = 0;
+		if ( t > k ) return false;
+		else if ( t == k ) dem++;
+	} return ( dem == 1 );
+}
+
+void qlNhiPhan ( int i ) {
+	for ( int j = 0; j <= 1; j++ ) {
+		x.at ( i ) = j;
+		if ( i == n - 1 ) {
+			if ( Check () )	Update ();
+		} else qlNhiPhan ( i + 1 );
+	}
+}
+
+int main () {
+	cin >> n >> k;
+	x.resize ( n );
+	qlNhiPhan ( 0 );
+	cout << d << endl;
+	res ();
+	system ( "pause" );
+}
