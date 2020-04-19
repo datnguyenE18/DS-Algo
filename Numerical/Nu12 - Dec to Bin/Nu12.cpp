e6e8c1fb93ca9a58256654de8c07e2c8a6c60d9b
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-19 13:17:36 +0700

    C++

---
+++ Numerical/Nu12 - Dec to Bin/Nu12.cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int deci; // Decimal Numbering System : Hệ đếm thập phân
+stack <int> bin; // Binary Numbering System
+
+void Dec_to_Bin () {
+	while ( deci ) {
+		bin.push ( deci % 2 );
+		deci /= 2;
+	}
+	int l = bin.size ();
+	while ( l-- ) {
+		cout << bin.top ();
+		bin.pop ();
+	} cout << "\n";
+}
+
+int main () {
+	cin >> deci;
+	Dec_to_Bin ();
+	system ( "pause" );
+}
---
+++ Numerical/Nu13 - Bin to Dec/Nu13.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+string bin;
+
+int Bin_to_Dec () {
+	int n = 0;
+	for ( int i = 0; i < 32 && bin[i]; i++ ) {
+		n <<= 1;
+		if ( bin[i] == '1' )
+			n |= 1;
+	} return n;
+}
+
+int main () {
+	cin >> bin;
+	cout << Bin_to_Dec () << "\n";
+	system ( "pause" );
+}
