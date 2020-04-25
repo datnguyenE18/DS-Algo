8da1a5cf417c44c7eedec15f0a6cae32079bc5d3
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-25 17:37:25 +0700

    C++

---
+++ 4. Generation/Ge2.9 - Tìm bội số chỉ gồm 0 và 9/Ge2.9.cpp
@@ -0,0 +1,33 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int n, //Số nguyên N
+	res[505]; //res[i] chứa số nhỏ nhất chỉ chứa 0 và 9 chia hết cho i
+bool visited[505]; 
+
+void Init () {
+	int dem = 0;
+	queue<int> q;
+	q.push ( 9 );
+	while ( dem < 500 ) { //Mỗi lần xét là một bộ các số gồm 0 và 9 có độ dài tăng dần
+		int t = q.front (); q.pop ();
+		for ( int i = 1; i <= 500 && i <= t; i++ ) {
+			if ( t % i == 0 && !visited[i] ) { //nếu t chia hết cho i thì visited[i] kiểm tra xem trước đó đã có số nào nhỏ hơn chia hết cho i chưa
+				visited[i] = true; //Nếu chưa thì đánh dấy là có rồi
+				res[i] = t; // Và thêm t vào res[i] xác nhận t lúc này là giá trị nhỏ nhất chia hết cho i
+				dem++;
+			}
+		}
+		q.push ( t * 10 ); //Lần lượt thêm 0
+		q.push ( t * 10 + 9 ); // và 9 vào đằng sau cấu hình trước để xác định cấu hình tiếp theo
+	}
+}
+
+int main () {
+	int t; cin >> t;
+	Init (); //Liệt kê tất cả các số nhỏ nhất res[i] chia hết cho i từ 1 đến 500
+	while ( t-- ) {
+		cin >> n;
+		cout << res[n] << endl; //gọi ra res[i] là số nhỏ nhât chia hết cho i
+	} system ( "pause" );
+}
---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB2.2 - Tìm bội số chỉ gồm 0 và 9/RB2.2.cpp
@@ -0,0 +1,44 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int m, //sinh xâu nhị phân có độ dài m = (1,2,3,...) 
+n, // số nguyên N (1 ≤ N ≤ 500)
+a[100]; //Lưu tạm kết quả nhị phân vào các ô để chuyển sang int
+
+bool stop; //kiểm soát vòng lặp tăng m;
+long long res = 1e18; //1e18 = 1000000000000000000LL
+
+// Quay lui Sinh nhị phân:
+void Try ( int i ) {
+	for ( int val = 0; val <= 1; val++ ) { //val là các giá trị có thể gán cho các vị trí trong a
+		a[i] = val;
+		if ( i == m ) { //Nếu vị trí i là phần tử cuối của dãy, tức là đã sinh được 1 chuỗi nhị phân có độ dài m trong a rồi
+			long long s = 0; //Lưu tạm kết quả dạng int
+
+			//chuyển a[100] sang int rồi lưu vào s
+			for ( int l = 1; l <= m; l++ )
+				s = 10 * s + a[l]; //lúc này s chỉ chứa 0 và 1
+
+			s *= 9; //nhân lên để s chứa 0 và 9 đúng với yêu cầu đề bài
+
+			if ( s % n == 0 && s >= n ) { // s >= n để tránh kết quả là 0
+				res = min ( s, res ); //vì bộ các xâu nhị phân được sinh ra , chuyển sang int * 9 thì có nhiều hơn 1 số chia hết cho n, nên ta chọn số nhỏ nhất gồm res được lưu trước đó và s mới
+				stop = true;
+			}
+		} else Try ( i + 1 ); //Nếu vị trí i chưa phải là phần tử cuối của dãy thì ta gán giá trị cho phần tử sau i
+	}
+}
+
+int main () {
+	int t; cin >> t;
+	while ( t-- ) {
+		m = 1;
+		stop = false;
+		res = 1e18; //Kết quả
+		cin >> n;
+		while ( !stop ) {
+			Try ( 1 ); // mỗi lượt Try là duyệt xong được 1 bộ xâu nhị phân có độ dài m 
+			m++;
+		} cout << res << "\n";
+	} system ( "pause" );
+}
