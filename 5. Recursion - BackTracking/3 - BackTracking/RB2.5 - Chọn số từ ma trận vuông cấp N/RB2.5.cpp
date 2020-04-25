f26e85b07fd2dc0763428c121740d6b7fb00a9ad
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-25 21:30:05 +0700

    C++

--- 5. Recursion - BackTracking/3 - BackTracking/RB2.5 - Chọn số từ ma trận vuông cấp N/RB2.5.cpp
+++ 5. Recursion - BackTracking/3 - BackTracking/RB2.5 - Chọn số từ ma trận vuông cấp N/RB2.5.cpp
@@ -1,28 +1,30 @@
 ﻿#include<bits/stdc++.h>
 using namespace std;
 
-vector<int> x ( 11 );
-vector<vector<int> > v;
-int a[11][11], n, k;
-bool check[11];
+vector<int> x ( 11 ); //Lưu vị trí
+vector<vector<int> > v; //Ma trận hai chiều lưu bộ các kết quả theo từ hàng
+int a[11][11], // lưu ma trận ban đầu
+	n, k;
+bool check[11]; // Kiểm tra xem giá trị cột này ở hàng đó đã xét qua chưa
 
 bool checkSum () {
 	int s = 0;
 	for ( int i = 1; i <= n; i++ )
-		s += a[i][x[i]];
+		s += a[i][x[i]]; //các hàng i cột x[i]
 	return ( s == k );
 }
 
 void qlHoanVi ( int i ) {
-	for ( int j = 1; j <= n; j++ )
-	if ( check[j] == false ) {
-		x[i] = j;
-		check[j] = true;
-		if ( i == n ) {
-			if ( checkSum () )
-				v.push_back ( x );
-		} else qlHoanVi ( i + 1 );
-		check[j] = false;
+	for ( int j = 1; j <= n; j++ ) {
+		if ( !check[j] ) { //Nếu cột này ở hàng đó chưa xét qua thì 
+			x[i] = j; //Lưu vị trí này vào x[i]
+			check[j] = true; //rồi lưu nó là đã xét
+			if ( i == n ) { // Nếu đã đến hàng cuối cùng rồi thì chuyển các giá trị trong ma trận v vừa lưu lên hàm checkSum 
+				if ( checkSum () )
+					v.push_back ( x );
+			} else qlHoanVi ( i + 1 ); //Nếu chưa đến hàng cuối cùng thì xét hàng tiếp theo
+			check[j] = false; //sau khi xét xong thì đánh dấu lại để phục vụ cho lần xét kế tiếp
+		}
 	}
 }
 
@@ -39,6 +41,5 @@ int main () {
 		for ( int j = 1; j <= n; j++ )
 			cout << v[i][j] << " ";
 		cout << endl;
-	}
-	system ( "pause" );
+	} system ( "pause" );
 }
