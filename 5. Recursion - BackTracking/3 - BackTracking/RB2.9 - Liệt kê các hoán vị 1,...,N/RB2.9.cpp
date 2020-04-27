1947b88de08016c7e6d27b8f854f636b54e009b4
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-27 07:53:16 +0700

    C++

---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB2.9 - Liệt kê các hoán vị 1,...,N/RB2.9.cpp
@@ -0,0 +1,32 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int n, X[20];
+bool chuaxet[21];
+
+void display () {
+	for ( int vt = 1; vt <= n; vt++ ) {
+		cout << X[vt];
+	} cout << " ";
+}
+
+void qlHoanVi ( int vt ) {
+	for ( int gt = 1; gt <= n; gt++ ) {
+		if ( chuaxet[gt] ) {
+			X[vt] = gt; chuaxet[gt] = false;
+			if ( vt == n ) display ();
+			else qlHoanVi ( vt + 1 );
+			chuaxet[gt] = true;
+		}
+	}
+}
+
+int main () {
+	int t; cin >> t;
+	while ( t-- ) {
+		memset ( chuaxet, true, 21 );
+		cin >> n;
+		qlHoanVi ( 1 );
+		cout << "\n";
+	} system ( "pause" );
+}
