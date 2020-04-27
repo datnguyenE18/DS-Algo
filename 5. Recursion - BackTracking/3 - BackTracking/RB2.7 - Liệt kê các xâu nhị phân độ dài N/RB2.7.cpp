a275a01cd66c6e8f2d4b4adb3088dd80d3e264cf
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-04-27 07:15:15 +0700

    C++

---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB2.7 - Liệt kê các xâu nhị phân độ dài N/RB2.7.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100
+
+int n, //Độ dài xâu nhị phân 
+	X[MAX]; //Xâu nhị phân X có độ dài tối đa là MAX = 100
+
+void display () {
+	for ( int i = 0; i < n; i++ )
+		cout << X[i];
+	cout << endl;
+}
+
+void qlNhiPhan ( int i ) { // Quay Lui Nhị Phân
+	for ( int j = 0; j < 2; j++ ) { // giá trị X có thể nhận là 1 và 0 (nhị phân)
+		X[i] = j; //thử giá trị X[i] có thể nhận
+		if ( i == n - 1 ) display (); //Nếu là phần tử cuối cùng rồi thì in ra
+		else qlNhiPhan ( i + 1 ); //Nếu chưa thì xét vị trí tiếp theo
+	}
+}
+
+int main () {
+	int t; cin >> t;
+	while ( t-- ) {
+		cin >> n;
+		qlNhiPhan ( 0 );
+	}
+	system ( "pause" );
+}
---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB2.8 - Liệt kê các tổ hợp chập k của n/RB2.8.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define MAX 20
+
+int n, k, X[MAX];
+
+void display () {
+	for ( int i = 1; i <= k; i++ ) {
+		cout << X[i];
+	} cout << " ";
+}
+
+void Try ( int i ) {
+	for ( int j = X[i - 1] + 1; j <= n - k + i; j++ ) {
+		X[i] = j;
+		if ( i == k ) {
+			display ();
+		} else Try ( i + 1 );
+	}
+}
+
+int main () {
+	int t; cin >> t;
+	while ( t-- ) {
+		X[0] = 0;
+		cin >> n >> k;
+		Try ( 1 );
+		cout << "\n";
+	} system ( "pause" );
+}
