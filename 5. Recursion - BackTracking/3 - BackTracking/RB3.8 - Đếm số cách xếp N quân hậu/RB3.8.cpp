2f3cb63d4cd80ebd6a78223e93da52dcea0ee768
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-05-03 20:50:24 +0700

    C++

--- 5. Recursion - BackTracking/3 - BackTracking/RB3.8 - Đếm số cách xếp N quân hậu/RB3.8.cpp
+++ 5. Recursion - BackTracking/3 - BackTracking/RB3.8 - Đếm số cách xếp N quân hậu/RB3.8.cpp
@@ -1,18 +1,17 @@
 ﻿#include<bits/stdc++.h>
 using namespace std;
-#define MAX 15
+#define MAX 100
 
-int n, 
-	dem, 
-	res[MAX], 
-	col[MAX], 
-	right_dia[2 * MAX], 
-	left_dia[2 * MAX]; 
+int n,
+	dem,
+	col[MAX], //Kiểm tra cột
+	right_dia[2 * MAX], //Kiểm tra đường chéo phải
+	left_dia[2 * MAX]; //Kiểm tra đường chéo trái
 
 void Init () {
-	dem = 0; 
+	dem = 0;
 	for ( int j = 1; j <= n; j++ )
-		col[j] = 1; 
+		col[j] = 1;
 
 	for ( int k = 1; k <= 2 * n - 1; k++ ) {
 		right_dia[k] = 1;
@@ -23,7 +22,6 @@ void Init () {
 void Try ( int i ) {
 	for ( int j = 1; j <= n; j++ ) {
 		if ( right_dia[i - j + n] && left_dia[i + j - 1] && col[j] ) {
-			res[i] = j;
 			col[j] = 0;
 			right_dia[i - j + n] = 0;
 			left_dia[i + j - 1] = 0;
