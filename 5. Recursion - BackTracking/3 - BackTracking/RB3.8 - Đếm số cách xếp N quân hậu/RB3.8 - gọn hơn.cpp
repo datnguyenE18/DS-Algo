1aa83ecc01b4a7f76eb4d81cdddda7159ce9d523
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-05-03 20:48:40 +0700

    C++

---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB3.8 - Đếm số cách xếp N quân hậu/RB3.8 - gọn hơn.cpp
@@ -0,0 +1,37 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+bool a[1000], xuoi[1000], nguoc[1000];
+long long ans = 0;
+
+void Try ( int i ) {
+	for ( int j = 1; j <= n; j++ ) {
+		if ( a[j] && xuoi[i - j + n] && nguoc[i + j - 1] ) {
+			a[j] = false;
+			xuoi[i - j + n] = false;
+			nguoc[i + j - 1] = false;
+			if ( i == n ) ans++;
+			else Try ( i + 1 );
+			a[j] = true;
+			xuoi[i - j + n] = true;
+			nguoc[i + j - 1] = true;
+		}
+	}
+}
+
+void init () {
+	for ( int i = 0; i < 1000; i++ ) {
+		a[i] = true;
+		xuoi[i] = true;
+		nguoc[i] = true;
+	}
+}
+
+int main () {
+	cin >> n;
+	init ();
+	Try ( 1 );
+	cout << ans << endl;
+	return 0;
+}
---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB3.8 - Đếm số cách xếp N quân hậu/RB3.8.cpp
@@ -0,0 +1,47 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define MAX 15
+
+int n, 
+	dem, 
+	res[MAX], 
+	col[MAX], 
+	right_dia[2 * MAX], 
+	left_dia[2 * MAX]; 
+
+void Init () {
+	dem = 0; 
+	for ( int j = 1; j <= n; j++ )
+		col[j] = 1; 
+
+	for ( int k = 1; k <= 2 * n - 1; k++ ) {
+		right_dia[k] = 1;
+		left_dia[k] = 1;
+	}
+}
+
+void Try ( int i ) {
+	for ( int j = 1; j <= n; j++ ) {
+		if ( right_dia[i - j + n] && left_dia[i + j - 1] && col[j] ) {
+			res[i] = j;
+			col[j] = 0;
+			right_dia[i - j + n] = 0;
+			left_dia[i + j - 1] = 0;
+			if ( i == n ) dem++;
+			else Try ( i + 1 );
+
+			col[j] = 1;
+			right_dia[i - j + n] = 1;
+			left_dia[i + j - 1] = 1;
+		}
+	}
+}
+
+
+int main () {
+	cin >> n;
+	Init ();
+	Try ( 1 );
+	cout << dem << "\n";
+	return 0;
+}
