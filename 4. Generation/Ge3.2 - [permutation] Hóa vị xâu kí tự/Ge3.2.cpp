65b8620bc5e0dbb6a119fa8caf41e3865e83e264
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-05-07 08:38:38 +0700

    C++

---
+++ 4. Generation/Ge3.2 - [permutation] Hóa vị xâu kí tự/Ge3.2.cpp
@@ -0,0 +1,35 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+string s;
+int n;
+char x[1000];
+
+void display() {
+	for (int i = 0; i < n; i++) cout << x[i];
+	cout << " ";
+}
+
+void init() {
+	n = s.size();
+	for (int i = 0; i < n; i++)
+		x[i] = s[i];
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		s = "";
+		cin >> s;
+		init();
+		while (prev_permutation(x, x + n)) {
+			display();
+		}
+		init();
+		do {
+			display();
+		} while (next_permutation(x, x + n));
+		cout << "\n";
+	}
+	return 0;
+}
