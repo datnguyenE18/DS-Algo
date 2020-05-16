bdd3fb3d3878f44e9729eb7cc9df8bffadbfdc40
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-05-16 11:37:55 +0700

    C++

---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB4.3 - Di chuyển trong mê cung 1/[C2] RB4.3.cpp
@@ -0,0 +1,54 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1001
+
+int N,  // Số ô vuông
+	mat[MAX][MAX]; // ma trận 2 chiều lưu 
+vector<string> vt;
+string ans;
+
+void init() {
+	cin >> N;
+	for ( int i = 1; i <= N; i++ ) {
+		for ( int j = 1; j <= N; j++ )
+			cin >> mat[i][j];
+	}
+}
+
+void Try(int x, int y) {
+	if ( mat[1][1] == 0 || mat[N][N] == 0 )
+		return;
+
+	if ( x == N && y == N )
+		vt.push_back(ans);
+
+	if ( mat[x + 1][y] == 1 && x != N ) {
+		ans += 'D';
+		Try(x + 1, y);
+		ans.erase(ans.length() - 1);
+	}
+
+	if ( mat[x][y + 1] == 1 && y != N ) {
+		ans += 'R';
+		Try(x, y + 1);
+		ans.erase(ans.length() - 1);
+	}
+}
+
+void showPath() {
+	Try(1, 1);
+	if ( vt.empty() ) cout << "-1" << endl;
+	else {
+		for ( int i = 0; i < vt.size(); i++ )
+			cout << vt[i] << " ";
+		cout << endl;
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while ( t-- ) {
+		init();
+		showPath();
+	}
+}
---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB4.6 - DÃY CON TỔNG BẰNG K/RB4.6.cpp
@@ -0,0 +1,67 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1001
+
+int n, k, a[MAX], mark[MAX];
+bool exist; //Kiểm tra xem có tồn tại bộ số nào không
+
+void init() {
+	exist = false;
+	cin >> n >> k;
+	for ( int i = 0; i < n; i++ )
+		cin >> a[i];
+	sort(a, a + n);
+}
+
+void display() {
+	vector<int> ans;
+	ans.clear();
+
+	for ( int i = 0; i < n; i++ ) {
+		if ( mark[i] == 1 )
+			ans.push_back(a[i]);
+	}
+
+	cout << "[";
+	for ( int j = 0; j < ans.size() - 1; j++ ) {
+		cout << ans[j] << " ";
+	}
+	cout << ans[ans.size() - 1] << "] ";
+}
+
+void check() {
+	int sum = 0;
+	for ( int i = 0; i < n; i++ ) {
+		if ( mark[i] == 1 ) //Nếu đánh dấu tại vị trí thứ i (trong a[]) là 1 
+			sum += a[i]; //thì tính nó vào tổng
+	}
+
+	if ( sum == k ) {
+		exist = true;
+		display();
+	}
+}
+
+// Quay lui nhị phân, bit 1 thì tính vào tổng , bit 0 thì bỏ qua
+void qlNhiPhan(int i) {
+	for ( int bit = 1; bit >= 0; bit-- ) {
+		mark[i] = bit; // Đánh dấu Số tại vị trí thứ i trong mảng a[],
+		if ( i == n - 1 ) check(); // Nếu đã chọn đến cuối rồi thì chuyển lên hàm check xem tổng có = K hay không
+		else qlNhiPhan(i + 1); //Nếu chưa quét đến cuối thì xét tiếp
+	}
+}
+
+void solve() {
+	qlNhiPhan(0);
+	if ( !exist )
+		cout << "-1";
+	cout << endl;
+}
+
+int main() {
+	int t; cin >> t;
+	while ( t-- ) {
+		init();
+		solve();
+	}
+}
