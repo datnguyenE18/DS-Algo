99badde16231b7db63e463157865e6f64b30e47b
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-08-07 02:43:26 +0700

    07/08/2020

---
+++ Sorting_Algorithms/Sx 14 - [use Built-in Sort()] Sorting 1/Sx14.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+const int maxN = 1e3 + 1;
+int A[maxN], n;
+
+void init() {
+	cin >> n;
+	for ( int i = 0; i < n; i++ )
+		cin >> A[i];
+}
+
+void solve() {
+	sort(A, A + n);
+	int l = 0, r = n - 1; // left & right
+	while ( l < r ) {
+		cout << A[r] << " " << A[l] << " ";
+		l++;
+		r--;
+	}
+	if ( l == r ) cout << A[l] << endl;
+}
+
+int main() {
+	int t; cin >> t;
+	while ( t-- ) {
+		init();
+		solve();
+	}
+}
