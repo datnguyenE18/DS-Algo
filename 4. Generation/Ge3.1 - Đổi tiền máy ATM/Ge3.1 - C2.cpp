8cb74f3ef048aff3eaa79d62c7d1fb07d542c0e4
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-08-08 11:17:26 +0700

    08/08/2020

---
+++ 4. Generation/Ge3.1 - Đổi tiền máy ATM/Ge3.1 - C2.cpp
@@ -0,0 +1,36 @@
+﻿#include<bits/stdc++.h>
+#define faster() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long i64;
+
+int n, s;
+vector < int > a;
+
+void init() {
+	cin >> n >> s; a.resize(n);
+	for ( int i = 0; i < n; i++ )
+		cin >> a[i];
+}
+
+void solve() {
+	sort(a.begin(), a.end());
+	int res = 0;
+	for ( int i = n - 1; i >= 0; i-- ) {
+		if ( s > a[i] ) {
+			int tmp = s / a[i];
+			s -= tmp * a[i];
+			res += tmp;
+		}
+	}
+	cout << res;
+}
+
+int main() {
+	faster();
+	int t = 1; cin >> t;
+	while ( t-- ) {
+		init();
+		solve();
+	}
+	//cerr << "\nRunning is : " << 1.0 * clock() / 1000;
+}
---
+++ 5. Recursion - BackTracking/3 - BackTracking/RB4.2 - Tam giác tổng/Tam giác xuôi/RB4.2.cpp
@@ -0,0 +1,59 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ONLINE_JUDGE "ONLINE_JUDGE"
+#define setIO() ios::sync_with_stdio(false); cin.tie(0);
+#define all(a) a.begin(),a.end()
+
+int test = 1, MULTI_TEST = 1, n;
+vector < int > a;
+vector < vector<int> > res;
+
+void Try(vector < int > b) {
+	vector < int > c;
+	res.push_back(b);
+	for ( int i = 0; i < b.size() - 1; i++ )
+		c.push_back(b[i] + b[i + 1]);
+	if ( c.size() != 0 ) Try(c);
+}
+
+void init() {
+	cin >> n; a.resize(n); res.clear();
+	for ( int i = 0; i < n; i++ )
+		cin >> a[i];
+}
+
+void solve() {
+	Try(a);
+	for ( int i = res.size() - 1; i >= 0; i-- ) {
+		cout << '[';
+		for ( int j = 0; j < res[i].size() - 1; j++ )
+			cout << res[i][j] << ' ';
+		cout << res[i].back() << ']' << "\n";
+	}
+	cout << '\n';
+}
+
+int main() {
+	setIO();
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	if ( MULTI_TEST == 1 ) cin >> test;
+	for ( int i = 1; i <= test; i++ ) {
+		init();
+		solve();
+}
+}
+
+/*
+1
+5
+1 2 3 4 5
+
+[48]
+[20 28]
+[8 12 16]
+[3 5 7 9]
+[1 2 3 4 5]
+*/
