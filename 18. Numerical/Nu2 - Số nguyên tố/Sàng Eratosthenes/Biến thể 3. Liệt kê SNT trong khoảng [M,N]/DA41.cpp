04cd7945ca20155c0b4cccca8972d74ea5e7bc4c
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-08-16 09:44:02 +0700

    16/08/2020

---
+++ 18. Numerical/Nu2 - Số nguyên tố/Sàng Eratosthenes/Biến thể 3. Liệt kê SNT trong khoảng [M,N]/DA41.cpp
@@ -0,0 +1,26 @@
+﻿#include <bits/stdc++.h> 
+using namespace std;
+
+void Eratosthenes(int m, int n) {
+	vector<bool>mark(n + 1, true);
+	mark[0] = mark[1] = false;
+
+	for ( int i = 2; i <= sqrt(n); i++ )
+		if ( mark[i] )
+			for ( int j = i * i; j <= n; j += i )
+				mark[j] = false;
+
+	for ( int i = m; i <= n; i++ )
+		if ( mark[i] )
+			cout << i << " ";
+	cout << endl;
+}
+
+int main() {
+	int m, n, t;
+	cin >> t;
+	while ( t-- ) {
+		cin >> m >> n;
+		Eratosthenes(m, n);
+	}
+}
