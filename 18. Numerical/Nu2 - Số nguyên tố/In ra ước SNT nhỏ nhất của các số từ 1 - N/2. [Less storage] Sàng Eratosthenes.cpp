c3be0b5a71a508f6b9d46ae0ce124a6f46ef30f0
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-08-16 09:34:53 +0700

    16/08/2020

---
+++ 18. Numerical/Nu2 - Số nguyên tố/In ra ước SNT nhỏ nhất của các số từ 1 - N/2. [Less storage] Sàng Eratosthenes.cpp
@@ -0,0 +1,39 @@
+﻿#include <bits/stdc++.h> 
+using namespace std;
+
+int n, t;
+
+void Eratosthenes(int n, vector<bool>mark) {
+	for ( int i = 2; i <= sqrt(n); i++ )
+		if ( mark[i] )
+			for ( int j = i * i; j <= n; j += i )
+				mark[j] = false;
+}
+
+int minMesure(int k, vector<bool>mark) {
+	for ( int i = 3; i <= n; i++ )
+		if ( mark[i] && ( k % i == 0 ) )
+			return i;
+}
+
+void solve() {
+	vector<bool> mark(n + 1, true);
+	Eratosthenes(n, mark);
+	for ( int i = 1; i <= n; i++ ) {
+		if ( i == 1 || i == 2 )
+			cout << i << " ";
+		else if ( i % 2 == 0 )
+			cout << 2 << " ";
+		else
+			cout << minMesure(i, mark) << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	cin >> t;
+	while ( t-- ) {
+		cin >> n;
+		solve();
+	}
+}
