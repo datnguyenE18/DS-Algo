0f0af5037134e427e7f61eb4134a68e635ac68c2
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-09-05 15:45:41 +0700

    05/09/2020

---
+++ 22. Modulo/1.5. Modulo 7 - Các phần tử A[] lấy phần dư với K đều bằng nhau/Mo7.cpp
@@ -0,0 +1,36 @@
+﻿#include<bits/stdc++.h> 
+using namespace std;
+
+int Solution(int A[], int n) {
+	sort(A, A + n);
+	int d = A[n - 1] - A[0];
+	vector <int> v;
+
+	for ( int i = 1; i * i <= d; i++ )
+		if ( d % i == 0 ) {
+			v.push_back(i);
+			if ( i != d / i )
+				v.push_back(d / i);
+		}
+
+	int ans = 0;
+	for ( int i = 0; i < v.size(); i++ ) {
+		int temp = A[0] % v[i];
+		int j;
+		for ( j = 1; j < n; j++ )
+			if ( A[j] % v[i] != temp )
+				break;
+		if ( j == n )
+			ans++;
+	}
+	return ans;
+}
+
+int main() {
+	int T, n, *A; cin >> T;
+	while ( T-- ) {
+		cin >> n; A = new int[n];
+		for ( int i = 0; i < n; i++ ) cin >> A[i];
+		cout << Solution(A, n) << endl;
+	}
+}
---
+++ 22. Modulo/1.6. Số nghiệm pt đồng dư x^2 = 1(mod) p trong khoảng [1,b]/Mo8.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solution(ll b, ll p) {
+	ll res = 0;
+	for ( ll i = 1; i < p; i++ ) {
+		if ( ( i * i ) % p == 1 ) {
+			ll last = i + p * ( b / p );
+			if ( last > b )
+				last -= p;
+			res += ( ( last - i ) / p + 1 );
+		}
+	}
+	cout << res << endl;
+}
+
+int main() {
+	int t; cin >> t;
+	ll b, p;
+	while ( t-- ) {
+		cin >> b >> p;
+		solution(b, p);
+	}
+}
