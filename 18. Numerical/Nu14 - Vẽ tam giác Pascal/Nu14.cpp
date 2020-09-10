4c17e35217f937477e2249e9608c98e55849fa4d
datnguyenE18 <43572616+datnguyenE18@users.noreply.github.com>
2020-09-10 09:11:20 +0700

    10/09/2020

---
+++ 18. Numerical/Nu14 - Vẽ tam giác Pascal/Nu14.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int fatorial(int n) {
+	int f;
+	for ( f = 1; n > 1; n-- )
+		f *= n;
+	return f;
+}
+
+int nCk(int n, int k) {
+	return fatorial(n) / ( fatorial(k) * fatorial(n - k) );
+}
+
+int main() {
+	int n;
+	cout << "So  hang n = "; cin >> n;
+	cout << "Tam gia Pascal\n";
+
+	for ( int i = 0; i <= n; i++ ) {
+		for ( int j = 0; j <= n - i; j++ )
+			cout << "  ";
+
+		for ( int j = 0; j <= i; j++ )
+			cout << "   " << nCk(i, j);
+
+		cout << endl;
+	}
+}
---
+++ 22. Modulo/1.7. nCr mod p/1.7.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+const unsigned int p = 1e9 + 7;
+
+void nCrModp(int n, int r) {
+	// Tam giác Pascal nCr
+	vector<int> C(r + 1, 0);
+	C[0] = 1; // Phần tử đầu tiên là 1
+
+	// Xây dựng tam giác Pascal từ C[0] = 1:
+	for ( int i = 1; i <= n; i++ )
+		for ( int j = min(i, r); j > 0; j-- )
+			// nCj = (n-1)Cj + (n-1)C(j-1); 
+			C[j] = ( C[j] + C[j - 1] ) % p;
+
+	cout << C[r] << endl;
+}
+
+int main() {
+	int t, n, r; cin >> t;
+	while ( t-- ) {
+		cin >> n >> r;
+		nCrModp(n, r);
+	}
+}
