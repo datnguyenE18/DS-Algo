9cf9555b4ca609d96481db81a7772c532da420b6
Nguyễn Đạt <43572616+datnguyenE18@users.noreply.github.com>
2022-09-22 23:34:03 +0700

    22/09/2022

--- 9. Sorting_Algorithms/Sx 7 - Sắp xếp Insertion Sort/Java/JvPrj.java
+++ 9. Sorting_Algorithms/Sx 7 - Sắp xếp Insertion Sort/Java/JvPrj.java
@@ -1,31 +1,45 @@
-package jvprj;
-
-import java.util.*;
+import java.util.Scanner;
 
 public class JvPrj {
 
-    public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int a[] = new int[n + 2];
-        for ( int i = 0; i < n; i++ ) {
-            a[i] = scanner.nextInt();
-        }
-        System.out.print("Buoc 0: " + a[0]);
-        System.out.println();
-        for ( int i = 1; i < n; i++ ) {
-            int value = a[i];
-            int j = i - 1;
-            while ( j >= 0 && a[j] > value ) {
-                a[j + 1] = a[j];
-                j--;
+    private static void insertion_Sort(int arr[], int n) {
+        // quét từng giá trị 1:
+        for (int i = 0; i < n; i++) {
+            // tìm vị trí thích hợp:
+            for (int j = 0; j <= i; j++) { // Quét từ đầu tới vị trí i đó
+                
+                // Nếu nhỏ hơn số nào là vị trí ở đó luôn:
+                if (arr[i] < arr[j]) {
+                    int save = arr[i], pos = j; // Lưu giá trị và vị trí của arr[i]
+                    
+                    // Dịch dần sang phải để chỗ lại cho arr[i]:
+                    for (int k = i - 1; k >= pos; k--)
+                        arr[k + 1] = arr[k];
+                    
+                    // Để arr[i] lại vị trí đã chọn:
+                    arr[pos] = save;
+                }
             }
-            a[j + 1] = value;
+
             System.out.print("Buoc " + i + ": ");
-            for ( int k = 0; k <= i; k++ ) {
-                System.out.print(a[k] + " ");
+            for (int k = 0; k <= i; k++) {
+                System.out.print(arr[k] + " ");
             }
             System.out.println();
         }
+
+    }
+
+    public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        // Nhập các phần tử array:
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        // Sắp xếp:
+        insertion_Sort(arr, n);
     }
 }
