31528afdeda6becc1a8097e298cc2e3a51f288f2
Nguyễn Đạt <43572616+datnguyenE18@users.noreply.github.com>
2022-12-16 01:55:25 +0700

    Add files via upload

---
+++ 16. String/Str20 - Đảo từ/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void reverseWordInMyString(String str) {
+        String[] words = str.split(" ");
+        String reversedString = "";
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            String reverseWord = "";
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+            reversedString = reversedString + reverseWord + " ";
+        }
+
+        System.out.println(reversedString);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+            String abc = sc.nextLine();
+            reverseWordInMyString(abc);
+        }
+    }
+}
